09925b355f7b3fbb2097cc61b6e83f3c757b3a43
Sean Blanton <seanblanton@gmail.com>
2018-05-23 23:50:02 -0700

    add permanent redirects for old slugs

--- public/sw.js
+++ public/sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-09baf724c1bbad37b1cb.js","973b08d336a91749c7093c408a5bb83e"],["/app-3cc3cb1d14fd0c2c5a93.js","08685b166f6317eb191da5847113136e"],["/app-5a04e56291d7063d3c59.js","8da3da87de053c6daa2675d012adedda"],["/app-9b0041b51c77d486bc18.js","fad2139bc84cfd942cedd318e7fdfd3e"],["/app-e8d3ed50f055a1fa0dd2.js","bdfcf450a8b0becf0494df10609b1f83"],["/commons-0c961af4250069caef0a.js","558c4be2d2dfd06b5b7014168c4450d5"],["/commons-e56f664354f4c00d2e02.js","498af0fb11c5ef9a948f663905650ac9"],["/index.html","59eca4b0b57f413d415022605033f682"],["/offline-plugin-app-shell-fallback/index.html","db088a06ecefddfd31a6bef09f29565a"]];
+var precacheConfig = [["/app-09baf724c1bbad37b1cb.js","973b08d336a91749c7093c408a5bb83e"],["/app-3cc3cb1d14fd0c2c5a93.js","08685b166f6317eb191da5847113136e"],["/app-5a04e56291d7063d3c59.js","8da3da87de053c6daa2675d012adedda"],["/app-9b0041b51c77d486bc18.js","fad2139bc84cfd942cedd318e7fdfd3e"],["/app-df148f5c9d0996f528e1.js","5c8eb7833f325a9d39af7fb88ebf596e"],["/app-e8d3ed50f055a1fa0dd2.js","bdfcf450a8b0becf0494df10609b1f83"],["/commons-0c961af4250069caef0a.js","558c4be2d2dfd06b5b7014168c4450d5"],["/commons-e56f664354f4c00d2e02.js","498af0fb11c5ef9a948f663905650ac9"],["/index.html","56a76a36ec373798c11d01b57a57e35d"],["/offline-plugin-app-shell-fallback/index.html","d1aff1f5b3d6277590f427a6407273b3"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
