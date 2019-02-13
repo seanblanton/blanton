44470961b294731df77d99b6c98c777ea04aef5f
Sean Blanton <seanblanton@Seans-MacBook-Air.local>
2019-02-12 18:04:21 -0800

    New post on system dynamics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,77 +1,78 @@
 module.exports = {
-	siteMetadata: {
-		title: 'Sean Blanton',
-		description:
-			'Sean Blanton is a Product Designer living in Seattle, Washington building design systems and tools at Facebook.',
-		siteUrl: `https://www.sean-blanton.com`
-	},
-	plugins: [
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `src`,
-				path: `${__dirname}/src/`
-			}
-		},
-		{
-			resolve: `gatsby-transformer-remark`,
-			options: {
-				plugins: [
-					{
-						resolve: `gatsby-remark-prismjs`,
-						options: {
-							classPrefix: 'language-',
-							inlineCodeMarker: '›',
-							aliases: {}
-						}
-					}
-				]
-			}
-		},
+  siteMetadata: {
+    title: "Sean Blanton",
+    description:
+      "Sean Blanton is a Product Designer living in Seattle, Washington building design systems and tools at Facebook.",
+    siteUrl: `https://www.sean-blanton.com`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: "›",
+              aliases: {}
+            }
+          }
+        ]
+      }
+    },
 
-		'gatsby-plugin-react-helmet',
-		`gatsby-plugin-styled-components`,
-		`gatsby-plugin-offline`,
-		`gatsby-remark-smartypants`,
-		{
-			resolve: `gatsby-plugin-sitemap`
-		},
-		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				trackingId: 'UA-29141738-1',
-				// Puts tracking script in the head instead of the body
-				head: false,
-				// Setting this parameter is optional
-				anonymize: true,
-				// Setting this parameter is also optional
-				respectDNT: true
-			}
-		},
-		{
-			resolve: `gatsby-plugin-nprogress`,
-			options: {
-				color: `#0ebcc7`,
-				showSpinner: false
-			}
-		},
-		{
-			resolve: `gatsby-plugin-favicon`,
-			options: {
-				logo: './src/favicon.png',
-				injectHTML: true,
-				icons: {
-					android: true,
-					appleIcon: true,
-					appleStartup: true,
-					coast: false,
-					favicons: true,
-					firefox: true,
-					twitter: false,
-					yandex: false,
-					windows: false
-				}
-			}
-		}
-	]
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
+    `gatsby-remark-smartypants`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sitemap`
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-29141738-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true
+      }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#0ebcc7`,
+        showSpinner: false
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
+  ]
 };
--- src/components/MarkdownBody.js
+++ src/components/MarkdownBody.js
@@ -1,161 +1,166 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import {
-	breakpoints,
-	colors,
-	fontSize,
-	spacing,
-	transition
-} from './../config';
+  breakpoints,
+  colors,
+  fontSize,
+  spacing,
+  transition
+} from "./../config";
 
 export const MarkdownBody = styled.div`
-	h1 {
-		color: ${colors.gray1};
-		font-size: ${fontSize.xl};
-		font-weight: 700;
-		line-height: 1.25;
-		margin: 0;
-		padding-top: 2rem;
-		padding-bottom: 1rem;
+  h1 {
+    color: ${colors.gray1};
+    font-size: ${fontSize.xl};
+    font-weight: 700;
+    line-height: 1.25;
+    margin: 0;
+    padding-top: 2rem;
+    padding-bottom: 1rem;
 
-		${breakpoints.medium} {
-			font-size: ${fontSize.xxl};
-		}
-	}
+    ${breakpoints.medium} {
+      font-size: ${fontSize.xxl};
+    }
+  }
 
-	h2 {
-		color: ${colors.gray1};
-		font-size: ${fontSize.m};
-		font-weight: 600;
-		line-height: 1.25;
-		margin: 2em 0 1em;
-		padding: 0;
+  h2 {
+    color: ${colors.gray1};
+    font-size: ${fontSize.m};
+    font-weight: 600;
+    line-height: 1.25;
+    margin: 2em 0 1em;
+    padding: 0;
 
-		${breakpoints.medium} {
-			font-size: ${fontSize.l};
-		}
-	}
+    ${breakpoints.medium} {
+      font-size: ${fontSize.l};
+    }
+  }
 
-	h3 {
-		color: ${colors.gray1};
-		font-size: ${fontSize.m};
-		font-weight: 400;
-		line-height: 1.25;
-		margin: 2em 0 1em;
-		padding: 0;
+  h3 {
+    color: ${colors.gray1};
+    font-size: ${fontSize.s};
+    font-weight: 400;
+    line-height: 1.5;
+    margin: 2em 0 1em;
+    padding: 0;
 
-		${breakpoints.medium} {
-			font-size: ${fontSize.l};
-		}
-	}
+    ${breakpoints.medium} {
+      font-size: ${fontSize.m};
+    }
+  }
 
-	h4 {
-		color: ${colors.gray1};
-		font-size: ${fontSize.s};
-		font-weight: 600;
-		line-height: 1.25;
-		margin: 2em 0 0.5em;
-		padding: 0;
+  h4 {
+    color: ${colors.gray1};
+    font-size: ${fontSize.s};
+    font-weight: 600;
+    line-height: 1.25;
+    margin: 2em 0 0.5em;
+    padding: 0;
 
-		${breakpoints.medium} {
-			font-size: ${fontSize.m};
-		}
-	}
+    ${breakpoints.medium} {
+      font-size: ${fontSize.m};
+    }
+  }
 
-	h5 {
-		color: ${colors.gray3};
-		font-size: ${fontSize.l};
-		font-weight: 400;
-		line-height: 1.25;
-		margin: 0;
-		padding: 0;
-	}
+  h5 {
+    color: ${colors.gray3};
+    font-size: ${fontSize.l};
+    font-weight: 400;
+    line-height: 1.25;
+    margin: 0;
+    padding: 0;
+  }
 
-	h6 {
-		color: ${colors.gray3};
-		font-size: ${fontSize.xs};
-		line-height: 1.25;
-		margin: 0;
-		padding: 0;
+  h6 {
+    color: ${colors.gray3};
+    font-size: ${fontSize.xs};
+    line-height: 1.25;
+    margin: 0;
+    padding: 0;
 
-		${breakpoints.medium} {
-			font-size: ${fontSize.s};
-		}
-	}
+    ${breakpoints.medium} {
+      font-size: ${fontSize.s};
+    }
+  }
 
-	p {
-		color: ${colors.gray1};
-		font-size: ${fontSize.s};
-		line-height: 1.75;
-		letter-spacing: 0;
-		margin: 0em 0em 2em;
-		padding: 0;
+  p {
+    color: ${colors.gray1};
+    font-size: ${fontSize.s};
+    line-height: 1.75;
+    letter-spacing: 0;
+    margin: 0em 0em 2em;
+    padding: 0;
 
-		${breakpoints.medium} {
-			font-size: ${fontSize.m};
-		}
-	}
+    ${breakpoints.medium} {
+      font-size: ${fontSize.m};
+    }
+  }
 
-	blockquote {
-		border-left: 2px solid ${colors.teal1};
-		font-style: italic;
-		margin: 3em 0;
-		padding: 0 1.5em;
-	}
+  blockquote {
+    border-left: 2px solid ${colors.teal1};
+    font-style: italic;
+    margin: 3em 0;
+    padding: 0 1.5em;
+  }
 
-	a {
-		border-bottom: 3px solid ${colors.teal0};
-		cursor: pointer;
-		color: ${colors.teal2};
-		padding-bottom: 2px;
-		text-decoration: none;
-		&:hover {
-			border-bottom: 3px solid ${colors.teal2};
-		}
-	}
+  a {
+    border-bottom: 3px solid ${colors.teal0};
+    cursor: pointer;
+    color: ${colors.teal2};
+    padding-bottom: 2px;
+    text-decoration: none;
+    &:hover {
+      border-bottom: 3px solid ${colors.teal2};
+    }
+  }
 
-	ul {
-		margin: 2em 0;
-	}
-	li {
-		padding-bottom: 1em;
-	}
+  ul {
+    margin: 2em 0;
+  }
+  li {
+    padding-bottom: 1em;
+  }
 
-	pre {
-		margin: 3em 0;
-	}
+  pre {
+    margin: 3em 0;
+  }
 
-	p,
-	ui,
-	li,
-	tr,
-	tr {
-		& code {
-			background-color: ${colors.teal0};
-			color: ${colors.teal2};
-			font-size: ${fontSize.m};
-			padding: 0.3em 0.5em !important;
-			margin: 0.2em;
+  p,
+  ui,
+  li,
+  tr,
+  tr {
+    & code {
+      background-color: ${colors.teal0};
+      color: ${colors.teal2};
+      font-size: ${fontSize.m};
+      padding: 0.3em 0.5em !important;
+      margin: 0.2em;
 
-			${breakpoints.medium} {
-				font-size: ${fontSize.m};
-			}
-		}
-	}
+      ${breakpoints.medium} {
+        font-size: ${fontSize.m};
+      }
+    }
+  }
 
-	table {
-		border-spacing: 0;
-	}
+  table {
+    border-spacing: 0;
+  }
 
-	thead {
-		font-weight: 700;
-	}
+  thead {
+    font-weight: 700;
+  }
 
-	td,
-	th {
-		border-collapse: separate;
-		border-bottom: 1px solid ${colors.gray4};
-		padding: 0.75em 1em;
-		text-align: left !important;
-	}
+  td,
+  th {
+    border-collapse: separate;
+    border-bottom: 1px solid ${colors.gray4};
+    padding: 0.75em 1em;
+    text-align: left !important;
+  }
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
