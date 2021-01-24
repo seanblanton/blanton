c1e9472a4282b54753e3ba336a8a561794bf3032
Sean Blanton <seanblanton@gmail.com>
2021-01-24 13:55:53 -0800

    lazy load google fonts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -40,11 +40,6 @@ const Layout = ({ children, showTitle }: Props) => (
       <meta charSet='utf-8' />
       <meta name='description' content={siteData.description} />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'
-        rel='stylesheet'
-      />
       <link rel='icon' href='favicon.svg' type='image/svg+xml' />
       <link rel='icon' href='/favicon.png' type='image/png' />
     </Head>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { AppProps } from "next/app";
-import React from "react";
+import React, { useEffect } from "react";
 import { DesignSystemProvider, ThemeProvider } from "@/system";
+import getFonts from "@/utils/fonts.js";
 
 import GlobalStyles from "@/components/GlobalStyles";
 
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    getFonts();
+  }, []);
+
   return (
     <ThemeProvider>
       <DesignSystemProvider>
